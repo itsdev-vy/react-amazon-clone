c82f073d802765aa76b38f1a8c69cd3f586c7d1a
Vinay <itsvinayadav@gmail.com>
2022-03-06 21:16:17 +0530

    Project Initialized and completed the Header UI

---
+++ src/Header.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import styled from 'styled-components';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const Header = () => {
+    return (
+        <Container>
+            <HeaderLogo>
+                <img src={"https://i.imgur.com/7I9Was5.png"} alt="" />
+            </HeaderLogo>
+
+            <HeaderOptionAddress>
+                <LocationOnIcon />
+                <HeaderOption>
+                    <OptionLineOne>Hello,</OptionLineOne>
+                    <OptionLineTwo>Select Your Address</OptionLineTwo>
+                </HeaderOption>
+            </HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type='text' />
+                <HeaderSearchIconContainer>
+                    <SearchIcon />
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+                <HeaderOption>
+                    <OptionLineOne>Hello Nazariy</OptionLineOne>
+                    <OptionLineTwo>Account & Lists</OptionLineTwo>
+                </HeaderOption>
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                </HeaderOption>
+                <HeaderOption>
+                    <HeaderOptionCart>
+                        <ShoppingBasketIcon />
+                        <CartCount>7</CartCount>
+                    </HeaderOptionCart>
+                </HeaderOption>
+            </HeaderNavItems>
+        </Container>
+    )
+}
+
+export default Header;
+
+const Container = styled.div`
+ height:60px;
+ background-color: #0F1111;
+ display:flex;
+ align-items:center;
+ justify-content:space-between;
+ color:white;
+`;
+
+const HeaderLogo = styled.div`
+ img{
+     width:100px;
+     margin-left:11px;  
+ }
+   
+`;
+
+const HeaderOptionAddress = styled.div`
+    padding-left: 9px; 
+    display: flex;
+    align-items: center;
+`;
+
+const OptionLineOne = styled.div`
+    
+`;
+
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`;
+
+const HeaderSearch = styled.div`
+    display:flex;
+    flex-grow:1; 
+    height:40px;
+    border-radius:4px;
+    overflow: hidden;
+    margin-left:4px;
+    background-color: white;
+    :focus-within{
+        box-shadow: 0 0 0 3px #F90;
+    }
+
+`;
+
+const HeaderSearchInput = styled.input`
+    flex-grow:1;
+    border:0; 
+
+    :focus{
+       outline:none;
+    }
+`;
+
+const HeaderSearchIconContainer = styled.div`
+    background-color:#fedb69; 
+    width:45px;
+    color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const HeaderNavItems = styled.div`
+    display:flex;
+`;
+
+const HeaderOption = styled.div`
+    padding: 10px 9px 10px 9px;
+`;
+
+const HeaderOptionCart = styled.div`
+    display:flex;
+    align-items: center;
+    padding-right: 9px;
+    padding-top:9px
+`;
+
+const CartCount = styled.div`
+    padding-left: 9px
+`;
+
+
